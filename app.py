b0fda12fd2b368cb41752c5b68bd35e4f95b6794
Haotong Wu <hw2933@nyu.edu>
2023-12-03 16:45:58 -0500

    111

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request, redirect, url_for, make_response
+from flask import Flask, render_template, request, redirect, url_for, make_response, flash, session
 from markupsafe import escape
 import pymongo
 import datetime
 from bson.objectid import ObjectId
 import os
 import subprocess
+
+
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+
+g_username=""
 
 # instantiate the app
 app = Flask(__name__)
@@ -27,8 +33,21 @@ connection = pymongo.MongoClient(config['MONGO_HOST'], 27017,
                                 password=config['MONGO_PASSWORD'],
                                 authSource=config['MONGO_DBNAME'])
 db = connection[config['MONGO_DBNAME']] # store a reference to the database
-
+users = connection[config['MONGO_DBNAME']]['users'] # store a reference to the users collection
 # set up the routes
+
+app.secret_key = 'your_secret_key'
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register', 'static']
+    if not is_logged_in() and request.endpoint not in allowed_routes:
+        return redirect(url_for('login'))
+
+def is_logged_in():
+    return 'username' in session
+ 
+
 
 @app.route('/')
 def home():
@@ -48,6 +67,7 @@ def read():
     return render_template('read.html', docs=docs) # render the read template
 
 
+
 @app.route('/create')
 def create():
     """
@@ -63,19 +83,59 @@ def create_post():
     Route for POST requests to the create page.
     Accepts the form submission data for a new document and saves the document to the database.
     """
-    name = request.form['fname']
+    #name = request.form['fname']
     message = request.form['fmessage']
 
 
     # create a new document with the data the user entered
     doc = {
-        "name": name,
+        "name": g_username,
         "message": message, 
         "created_at": datetime.datetime.utcnow()
     }
     db.exampleapp.insert_one(doc) # insert a new document
 
     return redirect(url_for('read')) # tell the browser to make a request for the /read route
+
+
+#login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = users.find_one({'username': username})
+        if user and check_password_hash(user['password'], password):
+            session['username'] = username  # 记录用户到 session
+            global g_username 
+            g_username=username
+            return redirect(url_for('read'))
+        elif not user:
+            flash('User not found')
+            return redirect(url_for('register'))
+        else:
+            flash('Wrong password')
+            return redirect(url_for('login'))
+    else:
+        return render_template('login.html')
+        
+
+#register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        password_hash = generate_password_hash(password)
+        users.insert_one({'username': username, 'password': password_hash})
+        return redirect(url_for('login'))  # 注册后重定向到登录页面
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 
 @app.route('/edit/<mongoid>')
@@ -97,6 +157,7 @@ def edit_post(mongoid):
     name = request.form['fname']
     message = request.form['fmessage']
 
+
     doc = {
         # "_id": ObjectId(mongoid), 
         "name": name, 
@@ -111,6 +172,13 @@ def edit_post(mongoid):
 
     return redirect(url_for('read')) # tell the browser to make a request for the /read route
 
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        query = request.form['query']
+        docs = db.exampleapp.find({"$or": [{"name": {"$regex": query}}, {"message": {"$regex": query}}]}).sort("created_at", -1)
+        return render_template('search.html', docs=docs)
+    return redirect(url_for('read'))
 
 @app.route('/delete/<mongoid>')
 def delete(mongoid):
